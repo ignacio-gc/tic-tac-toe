17fb31e4922e9c4c446af7311978f4e7288ed512
Ignacio Caamaño <ignacio167@gmail.com>
2020-11-17 13:19:01 -0300

    add pkg/

---
+++ pkg/tic_tac_toe.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @returns {any}
+*/
+export function initial_state_to_js(): any;
+/**
+* @returns {any}
+*/
+export function number(): any;
+/**
+* @param {any} board
+* @returns {number}
+*/
+export function solve(board: any): number;
---
+++ pkg/tic_tac_toe_bg.wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function initial_state_to_js(): number;
+export function number(): number;
+export function solve(a: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
